07c8fc6542a431ea12e9e583c00e07af05a6c7b6
zato91 <zatokey@gmail.com>
2020-07-22 18:41:49 -0400

    refacto and add bodyObject

--- src/index.js
+++ src/index.js
@@ -7,13 +7,12 @@ const fetchDogs = () => {
     .then(dogs => dogs.forEach(dog => renderDogs(dog) ))
 }
 
-//dogs.forEach(dog => console.log(dog))
 fetchDogs();
 
 function renderDogs(dog){
     
     const dogTable = document.querySelector('table');
-    let dogId = dog.id 
+    
     let dogTr = document.createElement('tr');
     dogTr.innerHTML = `<td>Dog ${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button id=${dog.id}>Edit</button></td> `
     dogTable.appendChild(dogTr);
@@ -32,9 +31,17 @@ function editDog(dog){
     dogForm.name.value = dog.name;  
     dogForm.breed.value = dog.breed;
     dogForm.sex.value = dog.sex ;
-    let top = dog.id;
+    
+
+    
+
     dogForm.addEventListener('submit', (e) => {
         e.preventDefault();
+
+        const name = dogForm.name.value 
+        const breed = dogForm.breed.value
+        const sex = dogForm.sex.value 
+         const bodyObject = {name, breed, sex}
 
         const options = {
                 method: "PATCH",
@@ -42,19 +49,15 @@ function editDog(dog){
                     "content-type": "application/json",
                     "accept": "application/json",
                 },
-                body: JSON.stringify({
-                    name: dogForm.name.value,
-                    breed: dogForm.breed.value,
-                    sex: dogForm.sex.value,
-                })
+                body: JSON.stringify(bodyObject)
 
         }
 
 
 
-        fetch(URL + top, options)
+        fetch(URL + dog.id, options)
             .then(resp => resp.json())
-            .then(dogs =>  renderDogs(dogs) )  
+            .then(renderDogs(dog) )  
         
      })
 
